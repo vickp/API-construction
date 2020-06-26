94b6d1a5f3a3fcda17e8925240081bb0fc8f114d
vickp <53214077+vickp@users.noreply.github.com>
2020-06-26 16:19:34 +0900

    Book API Added

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable linebreak-style */
 const Router = require('koa-router');
 
 const api = new Router();
+const books = require('./books');
 
 api.get('/books', (ctx, next) => {
     ctx.body = 'GET ' + ctx.request.path;
 });
-
 
 module.exports = api;
--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,29 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 const api = require('./api');
+const { route } = require('./api');
 
-router.use('./api', api.routes());
+router.use('/api', api.routes());
 
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.listen(8000, () => {
-    console.log('server is listening to port 8000');
+router.get('/', (ctx, next) => {
+    ctx.body = '홈';
 });
+
+router.get('/about', (ctx, next) => {
+    ctx.body = '소개';
+});
+
+router.get('/about/:name', (ctx, next) => {
+    const { id } = ctx.request.query;
+    if(id) {
+        ctx.body = '포스트 #' + id;
+    } else {
+        ctx.body = '포스트 아이디가 없습니다.';
+    }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+    console.log('server is listening to port 4000');
+})
