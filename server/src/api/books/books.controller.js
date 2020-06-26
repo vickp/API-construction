1cb9acccd91c7a90d98950f81f90841944d22b79
vickp <53214077+vickp@users.noreply.github.com>
2020-06-26 16:36:34 +0900

    Controller Export

---
+++ server/src/api/books/books.controller.js
@@ -0,0 +1,21 @@
+// exports.변수명 = ...
+
+exports.list = (ctx) => {
+    ctx.body = 'listed';
+};
+
+exports.create = (ctx) => {
+    ctx.body = 'created';
+};
+
+exports.delete = (ctx) => {
+    ctx.body = 'deleted';
+};
+
+exports.replace = (ctx) => {
+    ctx.body = 'replaced';
+};
+
+exports.update = (ctx) => {
+    ctx.body = 'updated';
+};
--- server/src/api/books/index.js
+++ server/src/api/books/index.js
@@ -1,19 +1,12 @@
 const Router = require('koa-router');
 
 const books = new Router;
+const booksCtrl = require('./books.controller');
 
-const handler = (ctx, next) => {
-    ctx.body = `${ctx.request.method} ${ctx.request.path}`
-}
-
-books.get('/', handler);
-
-books.post('/', handler);
-
-books.delete('/', handler);
-
-books.put('/', handler);
-
-books.patch('/', handler);
+books.get('/', booksCtrl.list);
+books.post('/', booksCtrl.create);
+books.delete('/', booksCtrl.delete);
+books.put('/', booksCtrl.replace);
+books.patch('/', booksCtrl.update);
 
 module.exports = books;
